3fddc9ab810bbc364a03442d184cb6cd666a6ea0
AUTHEMAN Victor <authirard.victor@gmail.com>
2020-04-02 19:11:39 +0200

    maj

--- Abalone/mainwindow.cpp
+++ Abalone/mainwindow.cpp
@@ -6,10 +6,50 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    initialisePlateau();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::initialisePlateau()
+{
+    /*
+    QPushButton* boutonbleuLigne1 = new QPushButton();
+    boutonbleuLigne1->setIcon(QIcon(QPixmap(":/new/icon/bleu")));
+    ui->gridLayoutPlateau->addWidget(boutonbleuLigne1,noLigne,noCol);
+    */
+
+    for(int noColone = 0; noColone < 17; noColone ++)
+    {
+        if(noColone != 4)
+        {
+            QPushButton* boutonbleuLigne1 = new QPushButton();
+            ui->gridLayoutPlateau->addWidget(boutonbleuLigne1,0,noColone);
+        }
+
+        if(noColone != 13)
+        {
+            int reste = noColone % 2;
+
+            if(reste != 0)
+            {
+                noColone ++;
+            } else {
+                QPushButton* boutonbleuLigne1 = new QPushButton();
+                boutonbleuLigne1->setIcon(QIcon(QPixmap(":/new/icon/bleu")));
+                ui->gridLayoutPlateau->addWidget(boutonbleuLigne1,0,noColone);
+            }
+        }
+
+        if(noColone > 13)
+        {
+            QPushButton* boutonbleuLigne1 = new QPushButton();
+            ui->gridLayoutPlateau->addWidget(boutonbleuLigne1,0,noColone);
+        }
+    }
+}
+
 
--- Abalone/mainwindow.h
+++ Abalone/mainwindow.h
@@ -15,6 +15,9 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void initialisePlateau();
+
 private:
     Ui::MainWindow *ui;
 };
